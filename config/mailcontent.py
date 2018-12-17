cb5c467d11a60f5587418414a6e878709c9523ff
MelanX <meissi97@me.com>
2018-12-17 18:31:12 +0100

    fixed recipients
    fixed each recipient listed in header from each recipient
    only one recipient shown (the right one)

--- config/mailcontent.py
+++ config/mailcontent.py
@@ -1,7 +1,3 @@
-# changes language to German
-# import locale
-# locale.setlocale(category=locale.LC_ALL, locale='German')
-
 # subject as string
 subject = ''
 
--- mail.py
+++ mail.py
@@ -5,23 +5,24 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from config.recipient import recipient
 
-msg = MIMEMultipart()
-msg['From'] = frommail
-msg['To'] = ', '.join(recipient)
-msg['Subject'] = subject
+for i in range(len(recipient)):
+    msg = MIMEMultipart()
+    msg['From'] = frommail
+    msg['To'] = recipient[i]
+    msg['Subject'] = subject
 
-# Text
-msgText = MIMEText(mail, 'html')
-msg.attach(msgText)
+    # Text
+    msgText = MIMEText(mail, 'html')
+    msg.attach(msgText)
 
-# Images
-# wow, nothing here yet!
+    # Images
+    # wow, nothing here yet!
 
-server = smtplib.SMTP(smtp, port)
-server.starttls()
-server.login(frommail, psw)
-text = msg.as_string()
-server.sendmail(frommail, recipient, text)
-server.quit()
+    server = smtplib.SMTP(smtp, port)
+    server.starttls()
+    server.login(frommail, psw)
+    text = msg.as_string()
+    server.sendmail(frommail, recipient[i], text)
+    server.quit()
 
-print('E-mail successfully sent.')
+print('E-mail(s) successfully sent.')
